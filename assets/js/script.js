913f11c0f892cc3cfa13a91a99c5b5cf3250bdc2
Vincent Toon <vincenttoon22@gmail.com>
2022-08-23 22:40:35 -0500

    so lost

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,17 +9,22 @@ let cityHum = document.getElementById('city-humid');
 let cityUv = document.getElementById('city-uv');
 let foreHead = document.getElementById('forecast-head');
 let fiveDay = document.getElementById('five-day-cont');
+const apiKey = "9231802aec25fba81f6ca09766ddefe6";
+
+// get current date
+
+// co
 
 // search cities for weather using API call
 let getCityInfo = function(lat, lon) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat={38.6270}&lon={90.1994}}&appid={9231802aec25fba81f6ca09766ddefe6}";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=38.6270&lon=90.1994&appid=9231802aec25fba81f6ca09766ddefe6";
     
     if (Response.ok) {
         console.log(lat, lon)
     }
 }
 
-https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&contd=&appid=${9231802aec25fba81f6ca09766ddefe6}
+// https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&contd=&appid=${9231802aec25fba81f6ca09766ddefe6}
 
 
 
@@ -30,4 +35,3 @@ https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=impe
 getCityInfo();
 
 // api key: 
-var apiKey = "9231802aec25fba81f6ca09766ddefe6";
