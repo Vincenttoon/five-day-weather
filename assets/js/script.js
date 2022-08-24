b36b7dee2ddcb7286ad1b061fd56b86021088e8b
Vincent Toon <vincenttoon22@gmail.com>
2022-08-23 23:19:37 -0500

    current day is in at least

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,26 +12,43 @@ let fiveDay = document.getElementById('five-day-cont');
 const apiKey = "9231802aec25fba81f6ca09766ddefe6";
 
 // get current date
+let currentDate = moment().format('MMMM Do YYYY');
+$("#currentDay").text(currentDate);
 
-// co
+// get info from city-search from submit button
+
+const handleForm = function() {
+    const city = $("#city-search").val().trim();
+}
+
+// use city info to move into lat, lon data
+
+let getLatLon = function(city) {
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?id=city&appid=${apiKey}';
+
+}
+
+// use lat, lon, city to generate weather
+
+// use lat, lon, city, weather info to generate results on html
+
+// generate daily results in 
+
+
 
 // search cities for weather using API call
-let getCityInfo = function(lat, lon) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=38.6270&lon=90.1994&appid=9231802aec25fba81f6ca09766ddefe6";
+// let getCityInfo = function(lat, lon) {
+//     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=38.6270&lon=90.1994&appid=9231802aec25fba81f6ca09766ddefe6";
     
-    if (Response.ok) {
-        console.log(lat, lon)
-    }
-}
+//     if (Response.ok) {
+//         console.log(lat, lon)
+//     }
+// }
 
 // https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&contd=&appid=${9231802aec25fba81f6ca09766ddefe6}
 
 
 
-// display city, state, temp, wind, humid, uv
-// display 5 day forecast
-// save city search data
-
-getCityInfo();
+// getCityInfo();
 
 // api key: 
